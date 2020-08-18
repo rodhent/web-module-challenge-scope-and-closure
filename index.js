a12184fc4f6419a6ff0f3ede64fad233d08bc1e8
Rod Hentringer <67014030+rodhent@users.noreply.github.com>
2020-08-18 11:52:28 -0400

    javascript and read-me  update

--- index.js
+++ index.js
@@ -34,11 +34,14 @@ function processFirstItem(stringList, callback) {
  * 2. Which of the two uses a closure? How can you tell?
  * 
  
-  
+    Counter1 because the count is defined.
 
  * 
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
- *
+ * 
+    If you are only using the variable for that function Counter1 is preferred. If you want to use the variable in multiple functions Counter2 would be preferred
+ * 
+ * 
 */
 
 // counter1 code
@@ -62,9 +65,12 @@ function counter2() {
 
 Write a function called `inning` that returns a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/) {
-  /*Code Here*/
+function inning() {
+  let score = Math.floor(Math.random() * 3);
+  return score;
 }
+
+console.log(inning());
 
 /* Task 3: finalScore()
 
@@ -80,9 +86,17 @@ finalScore(inning, 9) might return:
 
 */
 
-function finalScore(/*code Here*/) {
-  /*Code Here*/
+function finalScore(inning, i) {
+  let home = 0;
+  let away = 0;
+  for (let n = 0; n < i; n++) {
+    home += inning();
+    away += inning();
+  }
+  return `"Home": ${home},\n"Away": ${away}`;
 }
+
+console.log(finalScore(inning, 9));
 
 /* Task 4: 
 
@@ -104,6 +118,26 @@ and returns the score at each pont in the game, like so:
 9th inning: awayTeam - homeTeam
 Final Score: awayTeam - homeTeam */
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+function scoreboard(callBack, num) {
+  var home = 0;
+  var away = 0;
+  var inningNum = 0;
+
+  for (let i = 0; i < num; i++) {
+    home = home + inning();
+    away = away + inning();
+    inningNum = inningNum + 1;
+
+    if (inningNum === 1) {
+      console.log(`${inningNum}st inning: ${home} - ${away}`);
+    } else if (inningNum === 2) {
+      console.log(`${inningNum}nd inning: ${home} - ${away}`);
+    } else if (inningNum === 3) {
+      console.log(`${inningNum}rd inning: ${home} - ${away}`);
+    } else {
+      console.log(`${inningNum}th inning: ${home} - ${away}`);
+    }
+  }
+  return `Final Score: ${home} - ${away}`;
 }
+console.log(scoreboard(inning, 9));
